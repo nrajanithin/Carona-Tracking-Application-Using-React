9fb4c021a69880905c854e2fd616205fa912bdaf
nrajanithin <nrajanithin@gmail.com>
2020-11-25 10:49:54 +0530

    Updates

---
+++ precache-manifest.aac4ae1f242f6885cfb9a7f2bd52cd2e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10417afcd942849be54abd3415d53cd4",
+    "url": "/Carona-Tracking-Application-Using-React/index.html"
+  },
+  {
+    "revision": "69a1b5e13bdc22425dba",
+    "url": "/Carona-Tracking-Application-Using-React/static/css/main.6fc95a34.chunk.css"
+  },
+  {
+    "revision": "33dbd9bea5e97af8fe55",
+    "url": "/Carona-Tracking-Application-Using-React/static/js/2.0bf8abcc.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Carona-Tracking-Application-Using-React/static/js/2.0bf8abcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69a1b5e13bdc22425dba",
+    "url": "/Carona-Tracking-Application-Using-React/static/js/main.f1aad7e9.chunk.js"
+  },
+  {
+    "revision": "e6d591017cb7fca8c220",
+    "url": "/Carona-Tracking-Application-Using-React/static/js/runtime-main.c1ec2031.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Carona-Tracking-Application-Using-React/static/media/image.d7265326.png"
+  }
+]);
